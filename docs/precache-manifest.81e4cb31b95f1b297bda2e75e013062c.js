0c171d489651a75f0f852f2cbd284eeeb6d955f9
John Andrés <johnandrescv@gmail.com>
2020-07-14 18:05:43 -0500

    DOCS agregado

---
+++ docs/precache-manifest.81e4cb31b95f1b297bda2e75e013062c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df260dad1cb6b702aa402e533d1c1498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039dadca869c34783873",
+    "url": "/static/css/main.fc4694ef.chunk.css"
+  },
+  {
+    "revision": "5a83df518f2094aaf164",
+    "url": "/static/js/2.8fae794c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fae794c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039dadca869c34783873",
+    "url": "/static/js/main.b0a13ec0.chunk.js"
+  },
+  {
+    "revision": "fcdb2dd8f63dd0f52bfa",
+    "url": "/static/js/runtime-main.028153bf.js"
+  }
+]);
